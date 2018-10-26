6b948cd078776af8e9079215685fc29f4ecfcf54
RaoufEd <raouf.edabbar@gmail.com>
2018-10-26 19:04:13 +0100

    Add project files.

---
+++ Formation.Data/FormationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formation.Data
+{
+    public class FormationContext:DbContext
+    {
+        public FormationContext():base("name=ConnexionString")
+        {
+
+        }
+
+    }
+}
